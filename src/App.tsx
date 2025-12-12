48db467542fe019ba219c8546706692c48f21559
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-12-12 19:15:10 +0530

    Project Set-Up

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className="underline text-2xl ml-10 mt-10">App</div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Router>
+      {/*  BrowserRouter browser history API ko use karta hai - RELOAD SE BACHNE
+      ke liye! */}
+      <App />
+    </Router>
+  </StrictMode>
+);
