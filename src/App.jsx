9408648ca454894d84f7e3ec58f1695f8f04b26a
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-12-15 21:05:31 +0530

    06:benefits-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/benifits";
 import Header from "./components/header";
 import Hero from "./components/hero";
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero/>
+        <Benefits/>
       </div>
       <ButtonGradient />
     </>
